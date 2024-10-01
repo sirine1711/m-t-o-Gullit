f068b813d16d67a65f53d45a94b1a38456c16628
sirine1711 <sirine.elgaied@gmail.com>
2024-10-01 14:00:09 +0200

    V2

--- app/[ville]/page.jsx
+++ app/[ville]/page.jsx
@@ -1,205 +1,118 @@
-'use client'
+'use client';
+import { useState } from "react";
 import { Suspense } from "react";
 import Loading from "./loading";
 import './Weather.css';
+import Link from 'next/link';
 
+// Définition des icônes
+const icons = {
+  clear: 'assets/img/clear.png',
+  cloud: 'assets/img/cloud.png',
+  night: 'assets/img/night.png',
+  drizzle: 'assets/img/drizzle.png',
+  humidity: 'assets/img/humidity.png',
+  orage: 'assets/img/orage.png',
+  rain: 'assets/img/rain.png',
+  search: 'assets/img/search.png',
+  snow: 'assets/img/snow.png',
+  wind: 'assets/img/wind.png',
+};
 
-const clear = 'assets/img/clear.png'
-const cloud = 'assets/img/cloud.png'
-const night = 'assets/img/night.png'
-const drizzle = 'assets/img/drizzle.png'
-const humidity = 'assets/img/humidity.png'
-const orage = 'assets/img/orage.png'
-const rain = 'assets/img/rain.png'
-const search = 'assets/img/search.png'
-const snow = 'assets/img/snow.png'
-const wind = 'assets/img/wind.png'
-const backgrimg ='assets/img/Fond.png'
+// Composant de page
+const Pageville = ({ params }) => {
+  const [searchValue, setSearchValue] = useState('');
 
+  // Données de prévision dynamique
+  const forecast = [
+    { day: 'Mercredi', temp: 16, humidity: 91, windSpeed: '3.6 km/h', icon: icons.clear },
+    { day: 'Jeudi', temp: 16, humidity: 91, windSpeed: '3.6 km/h', icon: icons.clear },
+    { day: 'Vendredi', temp: 16, humidity: 91, windSpeed: '3.6 km/h', icon: icons.clear },
+    { day: 'Samedi', temp: 16, humidity: 91, windSpeed: '3.6 km/h', icon: icons.clear },
+    { day: 'Dimanche', temp: 16, humidity: 91, windSpeed: '3.6 km/h', icon: icons.clear },
+    { day: 'Lundi', temp: 16, humidity: 91, windSpeed: '3.6 km/h', icon: icons.clear },
+  ];
 
-const Pageville = ({params}) => {
-    return <Suspense fallback={<Loading/>}>
-        <h1>{params.ville}</h1>
-        <div className="backgrimg">
-      <div className="weather-row">
-        {/* Premier conteneur à gauche */}
-        <div className="weather">
-          <div className="search-bar">
-            <input type="text" placeholder="Search" />
-            <img src={search} alt="Search Icon" />
-          </div>
-          <img src={clear} alt="Clear Weather Icon" className="weather-icon" />
-          <p className="temperature">16°C</p>
-          <p className="location">{params.ville}</p>
-          <div className="weather-data">
-            <div className="col">
-              <img src={humidity} alt="Humidity Icon" />
-              <div>
-                <p>91%</p>
-                <span>Humidity</span>
-              </div>
-            </div>
-            <div className="col">
-              <img src={wind} alt="Wind Icon" />
-              <div>
-                <p>3.6 km/h</p>
-                <span>Wind Speed</span>
-              </div>
-            </div>
-          </div>
-          <p className="DateFR">Aujourd'hui</p>
-        </div>
+  const handleSearch = (e) => {
+    e.preventDefault();
+    // Ici on peut rediriger l'utilisateur vers la nouvelle ville avec le contenu de searchValue
+  };
 
-        {/* Conteneur pour les 6 petites cartes */}
-        <div className="weather-containers">
-          {/* Petite carte 1 */}
-          <div className="weather-container2">
-            <h1>Mercredi</h1>
-            <img src={clear} alt="Clear Weather Icon" className="weather-icon2" />
-            <p className="temperature2">16°C</p>
-            <p className="location2">{params.ville}</p>
-            <div className="weather-data2">
+  return (
+    <Suspense fallback={<Loading />}>
+      
+      
+      <div className="container-de-container">
+        <div className="weather-row">
+          
+          {/* Premier conteneur à gauche */}
+          <div className="weather">
+            <form onSubmit={handleSearch} className="search-bar">
+              <input 
+                type="text" 
+                placeholder="Search" 
+                value={searchValue} 
+                onChange={(e) => setSearchValue(e.target.value)} 
+              />
+              <Link href={`/${encodeURIComponent(searchValue)}`}>
+                <img src={icons.search} alt="Search Icon" />
+              </Link>
+            </form>
+
+            <img src={icons.clear} alt="Clear Weather Icon" className="weather-icon" />
+            <p className="temperature">16°C</p>
+            <p className="location">{params.ville}</p>
+            
+            <div className="weather-data">
               <div className="col">
-                <img src={humidity} alt="Humidity Icon" />
+                <img src={icons.humidity} alt="Humidity Icon" />
                 <div>
                   <p>91%</p>
                   <span>Humidity</span>
                 </div>
               </div>
               <div className="col">
-                <img src={wind} alt="Wind Icon" />
+                <img src={icons.wind} alt="Wind Icon" />
                 <div>
                   <p>3.6 km/h</p>
                   <span>Wind Speed</span>
                 </div>
               </div>
             </div>
+            <p className="DateFR">Aujourd'hui</p>
           </div>
 
-          {/* Petite carte 2 */}
-          <div className="weather-container2">
-            <h1>Jeudi</h1>
-            <img src={clear} alt="Clear Weather Icon" className="weather-icon2" />
-            <p className="temperature2">16°C</p>
-            <p className="location2">{params.ville}</p>
-            <div className="weather-data2">
-              <div className="col">
-                <img src={humidity} alt="Humidity Icon" />
-                <div>
-                  <p>91%</p>
-                  <span>Humidity</span>
+          {/* Conteneur pour les 6 petites cartes */}
+          <div className="weather-containers">
+            {forecast.map((day, index) => (
+              <div key={index} className="weather-container2">
+                <h1>{day.day}</h1>
+                <img src={day.icon} alt="Weather Icon" className="weather-icon2" />
+                <p className="temperature2">{day.temp}°C</p>
+                <p className="location2">{params.ville}</p>
+                <div className="weather-data2">
+                  <div className="col">
+                    <img src={icons.humidity} alt="Humidity Icon" />
+                    <div>
+                      <p>{day.humidity}%</p>
+                      <span>Humidity</span>
+                    </div>
+                  </div>
+                  <div className="col">
+                    <img src={icons.wind} alt="Wind Icon" />
+                    <div>
+                      <p>{day.windSpeed}</p>
+                      <span>Wind Speed</span>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="col">
-                <img src={wind} alt="Wind Icon" />
-                <div>
-                  <p>3.6 km/h</p>
-                  <span>Wind Speed</span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Petite carte 3 */}
-          <div className="weather-container2">
-            <h1>Vendredi</h1>
-            <img src={clear} alt="Clear Weather Icon" className="weather-icon2" />
-            <p className="temperature2">16°C</p>
-            <p className="location2">{params.ville}</p>
-            <div className="weather-data2">
-              <div className="col">
-                <img src={humidity} alt="Humidity Icon" />
-                <div>
-                  <p>91%</p>
-                  <span>Humidity</span>
-                </div>
-              </div>
-              <div className="col">
-                <img src={wind} alt="Wind Icon" />
-                <div>
-                  <p>3.6 km/h</p>
-                  <span>Wind Speed</span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Petite carte 4 */}
-          <div className="weather-container2">
-            <h1>Samedi</h1>
-            <img src={clear} alt="Clear Weather Icon" className="weather-icon2" />
-            <p className="temperature2">16°C</p>
-            <p className="location2">{params.ville}</p>
-            <div className="weather-data2">
-              <div className="col">
-                <img src={humidity} alt="Humidity Icon" />
-                <div>
-                  <p>91%</p>
-                  <span>Humidity</span>
-                </div>
-              </div>
-              <div className="col">
-                <img src={wind} alt="Wind Icon" />
-                <div>
-                  <p>3.6 km/h</p>
-                  <span>Wind Speed</span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Petite carte 5 */}
-          <div className="weather-container2">
-            <h1>Dimanche</h1>
-            <img src={clear} alt="Clear Weather Icon" className="weather-icon2" />
-            <p className="temperature2">16°C</p>
-            <p className="location2">{params.ville}</p>
-            <div className="weather-data2">
-              <div className="col">
-                <img src={humidity} alt="Humidity Icon" />
-                <div>
-                  <p>91%</p>
-                  <span>Humidity</span>
-                </div>
-              </div>
-              <div className="col">
-                <img src={wind} alt="Wind Icon" />
-                <div>
-                  <p>3.6 km/h</p>
-                  <span>Wind Speed</span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Petite carte 6 */}
-          <div className="weather-container2">
-            <h1>Lundi</h1>
-            <img src={clear} alt="Clear Weather Icon" className="weather-icon2" />
-            <p className="temperature2">16°C</p>
-            <p className="location2">{params.ville}</p>
-            <div className="weather-data2">
-              <div className="col">
-                <img src={humidity} alt="Humidity Icon" />
-                <div>
-                  <p>91%</p>
-                  <span>Humidity</span>
-                </div>
-              </div>
-              <div className="col">
-                <img src={wind} alt="Wind Icon" />
-                <div>
-                  <p>3.6 km/h</p>
-                  <span>Wind Speed</span>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
-    </div>
     </Suspense>
-    
-}
+  );
+};
 
 export default Pageville;
--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,7 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-
-
+import './globals.css';
 
 export default function MyFunction() {
   const [inputValue, setInputValue] = useState('');
@@ -13,20 +12,38 @@ export default function MyFunction() {
   };
 
   return (
-<div>
-    <div>
-    <div  onSubmit={handleSubmit}>
-      <input      
+    <div 
+      onSubmit={handleSubmit} 
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',  // Pour que le contenu soit centré verticalement sur toute la hauteur de la page
+        flexDirection: 'column'  // Pour aligner les éléments verticalement
+      }}
+    >
+      <input
         type="text" 
         value={inputValue} 
         onChange={(e) => setInputValue(e.target.value)} 
         placeholder="Tapez quelque chose"
+        style={{
+          marginBottom: '20px',  // Espace entre l'input et le bouton
+          padding: '10px',        // Un peu de padding pour rendre l'input plus large
+          fontSize: '16px'        // Taille du texte
+        }}
       />
-<Link href={`/${encodeURIComponent(inputValue)}`}>
-        <button  type="submit">Appuie</button>
+      <Link href={`/${encodeURIComponent(inputValue)}`}>
+        <button 
+          type="submit"
+          style={{
+            padding: '10px 20px',  // Ajuste la taille du bouton
+            fontSize: '16px'        // Taille du texte du bouton
+          }}
+        >
+          Appuie
+        </button>
       </Link>
     </div>
-  </div>
-</div>
   );
 }
